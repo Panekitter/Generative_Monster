483332ecaecb8170da68a4b214c1d79f466c9342
Panekitter <yamamoti2@gmail.com>
2025-01-06 18:06:57 +0900

    ログインボタンとログイン処理を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,16 @@ class User < ApplicationRecord
   has_many :characters, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 
   class << self
     def find_or_create_from_auth_hash(auth_hash)
       is_new_user = false
-      user = nil
-
-      ActiveRecord::Base.transaction do
-        user = find_or_create_by(email: auth_hash.info.email) do |user|
-          is_new_user = true
-          user.save!
-        end
-        user.update!(name: generate_username(user.id)) if user.name.blank?
+      user = find_or_initialize_by(email: auth_hash.info.email) do |user|
+        is_new_user = true
+        user.name = "temp"
+        user.save!
+        user.update!(name: generate_username(user.id))
       end
       [user, is_new_user]
     end
--- db/migrate/20250103083529_change_name_null_constraint_on_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNameNullConstraintOnUsers < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :users, :name, true
-  end
-end
